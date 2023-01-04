91862377f8321f202a4b2ee4b6695233d9d486bf
arkmadj <ahmadjinadu@gmail.com>
2023-01-04 17:21:21 +0100

    Added get all cars

--- src/components/cars/cars.resolver.ts
+++ src/components/cars/cars.resolver.ts
@@ -1,12 +1,15 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { CarsService } from './cars.service';
+import { Car } from './entities/cars';
 
 @Resolver()
 export class CarsResolver {
   constructor(private carsService: CarsService) {}
 
-  @Query((returns) => String)
-  public async cars() {
-    return 'Hello and welcome to MyCar shop.';
+  @Query((returns) => [Car])
+  public async cars(): Promise<Car[]> {
+    return await this.carsService.getAllCars().catch((err) => {
+      throw err;
+    });
   }
 }
