69b246f226378e261f129ebe961e54b41d0f9d1a
arkmadj <ahmadjinadu@gmail.com>
2023-01-05 23:52:32 +0100

    Added validation pipe

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new BaseExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
-  
+
   await app.listen(3000);
 }
 bootstrap();
