9c3dbf8404369726c603b8874b5e0f493d2487db
arkmadj <ahmadjinadu@gmail.com>
2023-01-05 23:52:07 +0100

    Added validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new BaseExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(3000);
 }
 bootstrap();
